6b6c429878447be0db5a1d490c410fa707a38262
charliemday <45036245+charliemday@users.noreply.github.com>
2023-07-23 20:30:25 +0000

    0.0.12

---
+++ photon/__init__.py
@@ -0,0 +1,3 @@
+from .photon import Photon
+from .processors import PhotonProcessor
+from .providers import OpenAIProvider
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "0.0.9"
+VERSION = "0.0.12"
 DESCRIPTION = ""
 LONG_DESCRIPTION = ""
 
